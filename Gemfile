479e853c883759dff5255f6832f477c6dedd4e1d
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-05 12:15:35 -0700

    add bcrypt dependencies

--- Gemfile
+++ Gemfile
@@ -21,3 +21,5 @@ group :test do
   gem 'faker'
   gem 'rspec'
 end
+
+gem 'bcrypt-ruby'
--- config/environment.rb
+++ config/environment.rb
@@ -32,3 +32,4 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
 
+require 'bcrypt'
